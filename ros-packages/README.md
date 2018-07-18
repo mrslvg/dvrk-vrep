14e9b9b8ce2d93f1c133504c44a20282e6ea2570
mselvagg <mar.selvaggio@gmail.com>
2018-07-18 11:40:57 +0200

    README ros packages

---
+++ ros-packages/README.md
@@ -0,0 +1,22 @@
+V-REP simulator for the da Vinci Research Kit (dVRK)
+====================
+This repository contains three ros packages:
+
+* blob-tracker: performs tracking of features in the simulator images
+* visual-servoing: contains the robot control functions
+* needle-tracker: performs tracking of the needle
+
+# Dependences
+* Download and install ROS http://www.ros.org/
+* Download and install visp http://visp.inria.fr/
+
+
+Create a catkin_ws and download the packages in your src folder.
+
+Additionally you need the visp_ros package  http://wiki.ros.org/vision_visp
+
+# Build
+* Build using catkin build.
+
+
+
--- ros-packages/visual-servoing/visual_servoing_ecm.cpp
+++ ros-packages/visual-servoing/visual_servoing_ecm.cpp
@@ -3,8 +3,6 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
-#include "ros/ros.h"
-#include "std_msgs/String.h"
 #include <sensor_msgs/JointState.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PointStamped.h>
@@ -15,33 +13,30 @@
 #include <eigen3/Eigen/SVD>
 #include <sstream>
 
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
 geometry_msgs::PoseStamped pose;
 bool msg_received = false;
-//Eigen::Matrix<double,6,4> jacobian;
 Eigen::Matrix<double,6,1> twist_des;
 std::vector<double> joint_state_current;
 Eigen::Matrix<double,3,3> R;
 Eigen::Matrix<double,3,1> p;
 Eigen::Matrix<double,3,1> rce;
 
-
 void cartesianVelocityDesiredCallback(const geometry_msgs::Twist& msg)
 {
   twist_des << msg.linear.x,msg.linear.y,msg.linear.z,msg.angular.x,msg.angular.y,msg.angular.z;
-  //std::cout<<pose<<std::endl;
 }
 
 void objectPoseCallback(const geometry_msgs::PoseStamped& msg)
 {
-  
   pose = msg;
-  //std::cout<<pose<<std::endl;
 }
 
 void jointStateCallback(const sensor_msgs::JointState& msg)
 {
     msg_received = true;
-    std::cout<<"joint_state_current is: ";
     joint_state_current.clear();
     for (unsigned i=0; i<msg.position.size(); i++)\
     {
@@ -140,48 +135,42 @@ Eigen::Matrix<double,6,1> invertTwist(Eigen::Matrix<double,6,1> twist)
 Eigen::Matrix<double,6,1> rotateTwist(Eigen::Matrix<double,6,1> twist, Eigen::Matrix<double,3,3> R, Eigen::Matrix<double,3,1> p)
 {
   //twist is given in [v,w] form and returned in the same form
-  //Eigen::Matrix<double,6,1> invertedTwist = invertTwist(twist);
   Eigen::Matrix<double,6,1> rotatedTwist;
-  Eigen::Matrix<double,6,1> endEffectorTwist;
   Eigen::Matrix<double,6,6> X;
   X.setZero();
   X.block<3,3>(0,0) = R;
   X.block<3,3>(3,3) = R;
   rotatedTwist = X*twist;
 
-  std::cout << "X1: " << std::endl << X << std::endl;
   Eigen::Matrix3d S;
   S << 0, -p[2], p[1], p[2], 0, -p[0], -p[1], p[0], 0;
   
   X.setIdentity();
   X.block<3,3>(0,3) = -S;
-  std::cout << "X2: " << std::endl << X << std::endl;
-  endEffectorTwist = (rotatedTwist.transpose()*X).transpose();
-  //rotatedTwist = X*invertedTwist;
-  //rotatedTwist = invertTwist(rotatedTwist);
-  return endEffectorTwist;   
+  rotatedTwist = (rotatedTwist.transpose()*X).transpose();
+  return rotatedTwist;   
 }
 
 int main(int argc, char **argv)
 {
     //Initiate the Ros Publishers
-    std::ofstream camera_position_file;
-    camera_position_file.open ("camera_position.txt");
+    
     ros::init(argc, argv, "ros_node_visual_servoing");
     ros::NodeHandle n_1;
     ros::Subscriber object_pose_sub = n_1.subscribe("object_1/pose", 1, objectPoseCallback);
     ros::Subscriber des_velocity_sub = n_1.subscribe("/dvrk/ECM/cartesian_velocity_desired", 1, cartesianVelocityDesiredCallback);
     ros::Subscriber joint_state_sub = n_1.subscribe("/dvrk/ECM/state_joint_current", 1, jointStateCallback);
     ros::Publisher joint_state_pub = n_1.advertise<sensor_msgs::JointState>("/dvrk/ECM/state_joint_desired", 1);
-    sensor_msgs::JointState ecm_des_jnts;
+    
     ros::Rate loop_rate(100);
+    
+    sensor_msgs::JointState ecm_des_jnts;
+
     int count = 0;
-    Eigen::MatrixXd jacp;
-    jacp.resize(4,6);
-    float k = 0.5;
+    Eigen::Matrix<double,4,6> jacp;
     Eigen::Matrix3d K;
     K.setIdentity();
-    K = k*K; 
+    K = 0.5*K; 
     std::vector<double> q_std;
     ros::Duration(1).sleep();
     rce << -0.0025, 0, -0.0035;
@@ -194,48 +183,23 @@ int main(int argc, char **argv)
     {
         if (msg_received)
         {   
-            //retrieve generalized coordinates
+            //update robot generalized coordinates
             q_std = joint_state_current;
 
             Eigen::Vector4d q_init(q_std.data());
-
-            //update kinematics and jacobian
-            // std::vector<double> zero;
-            // zero.resize(4);
-            // zero.assign(zero.size(),0);
             forwardKinematicsECM(q_std);
             jacobian = ecm_jacobian(q_std);
-
-
-            std::cout<<"R: " << R <<std::endl;
-            std::cout<<"p: " << p <<std::endl;
-            std::cout << "size: " << jacobian.size() << std::endl; 
-            std::cout<<std::endl <<"J: " << std::endl << jacobian <<std::endl;
-            
             Eigen::Matrix<double,6,1> twist_base = rotateTwist(twist_des, R, rce);
 
-            //choose pseudo inverse or transpose
             jacp = pseudoinverse(jacobian);
 
-            std::cout<<std::endl <<"J pinv: " << std::endl << jacp <<std::endl;
-
-            //jacp = jacobian.transpose();
-            std::cout<< std::endl <<"twist_base: " << twist_base <<std::endl;
             Eigen::Vector4d dq = jacp*twist_base;
             q_eigen = q_init+dq*0.01;
             q_std.resize(q_eigen.size());
-            std::cout<< std::endl <<"--------------------q_eigen: " << q_eigen <<std::endl;
-
 
             Eigen::VectorXd::Map(&q_std[0],q_eigen.size()) = q_eigen;
             sensor_msgs::JointState joint_state_desired;
             joint_state_desired.position = q_std;
-            camera_position_file << p[0] << ' '<< p[1] << ' ' << p[2] << std::endl;
-            // std::cout<<"joint_state_desired is: " << q_init+jacp*c_vel_des <<std::endl;
-            // for (unsigned i=0; i<q_std.size(); i++)
-            // {
-            //     std::cout << joint_state_desired.position[i] <<std::endl;
-            // }
             joint_state_pub.publish(joint_state_desired);
 
         }
@@ -244,10 +208,3 @@ int main(int argc, char **argv)
     }
 return 0;
 }
-
-            // Eigen::Vector3d xd;
-            // xd << pose.pose.position.x,pose.pose.position.y,0.1;
-            // Eigen::Vector3d x;
-            // x << pose.pose.position.x,pose.pose.position.y,pose.pose.position.z;
-            // Eigen::Vector3d e = xd - x;
-            //Eigen::Vector4d joints(joint_state.data());
